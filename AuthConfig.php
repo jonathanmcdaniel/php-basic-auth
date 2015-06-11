5db4118d4c66b2c1a8cf2f85232e63aa0103e1ea
Jonathan McDaniel <jonathanmcdaniel@users.noreply.github.com>
2015-06-11 14:44:40 -0400

    Update AuthConfig.php

--- AuthConfig.php
+++ AuthConfig.php
@@ -1,5 +1,7 @@
 <?php
 
+/* Configuration file for php-basic-auth. Be sure to configure the file to match the needs of your database and use-case. */
+
 class AuthConfig {
 
   /* Database Configurations */
@@ -8,6 +10,21 @@ class AuthConfig {
   public $databaseUser = "root";        // Username with access to the database:              REQUIRED
   public $databasePassword = "root";    // Password for the username                          REQUIRED
   public $databaseName = "";            // Name of the database at the host being accessed:   REQUIRED
+  
+
+  /* Security Settings */
+  public $didUsePasswordHashing = true; // Did you use the password_hash() function?          REQUIRED
+  
+  
+  /* SQL Prepared Statement */
+  /* Write the SQL Statement to Match Your Needs. All information will be returned in an object */
+  public $sqlStatement = "SELECT * FROM users WHERE username=? LIMIT 1";
+  
+  
+  /* User Information */
+  /* DO NOT MODIFY THIS SECTION */
+  public $user = $_SERVER['PHP_AUTH_USER'];
+  public $password = $_SERVER['PHP_AUTH_PW'];
   
 }
 
