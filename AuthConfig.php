1da1934714fea935c03a3add7cd8496316566d4b
Jonathan McDaniel <jonathanmcdaniel@users.noreply.github.com>
2015-06-11 13:52:24 -0400

    Create AuthConfig.php

---
+++ AuthConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+class AuthConfig {
+
+  /* Database Configurations */
+  public $databaseHost = "127.0.0.1";   // URL or IP where database is located:               REQUIRED
+  public $databasePort = "8889";        // Port where database can be accessed on the host:   OPTIONAL
+  public $databaseUser = "root";        // Username with access to the database:              REQUIRED
+  public $databasePassword = "root";    // Password for the username                          REQUIRED
+  public $databaseName = "";            // Name of the database at the host being accessed:   REQUIRED
+  
+}
+
+?>
