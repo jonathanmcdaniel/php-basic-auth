6b74e63b2aee4c50f7a8dbeb01b477294a424064
Jonathan McDaniel <jonathanmcdaniel@users.noreply.github.com>
2015-06-12 10:05:02 -0400

    Update BasicAuth.php

--- BasicAuth.php
+++ BasicAuth.php
@@ -1,14 +1,38 @@
 <?php
 
+
 require __DIR__ . '/AuthConfig.php';
 
 
 class BasicAuth {
-
+  
+  $user;
+  $password;
+  
+  
+  // Authenticate the User
+  public function authenticate ($user = $_SERVER['PHP_AUTH_USER'], $password = $_SERVER['PHP_AUTH_PW']) {
+    
+    // Set Object Variables
+    $this->user = $user;
+    $this->password = $password;
+    
+  }
+  
+  
+  // Ensure hat Configurations are Properly Set
+  private function validateConfigSettings () {
+    
+  }
+  
+  
+  // Retrieve the Particular User from the Database
+  private function retrieveUser () {
+    
+  }
 
 
 }
-
 
 
 ?>
